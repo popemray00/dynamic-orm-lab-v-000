4e2b93c6eb5ff1e50c23e1d4741e581dd116c1e2
Michael Pope <popemray1986@gmail.com>
2018-03-05 04:59:08 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,4 +52,9 @@ class InteractiveRecord
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
+
+  def self.find_by(row)
+    sql = "SELECT * FROM #{self.table_name}"
+
+  end
 end
